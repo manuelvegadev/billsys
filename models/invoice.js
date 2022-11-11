67282918c7cb1e4c19a12e72763c6c793b89251e
Manuel Vega <manuelvega.dev@gmail.com>
2022-11-10 23:32:24 -0500

    feat(models): Created `Invoice` and `InvoiceItem` types

---
+++ models/invoice.js
@@ -0,0 +1,6 @@
+/**
+ * @typedef {Object} InvoiceType
+ * @property {string} title
+ * @property {number} number
+ * @property {string} date
+ * */
---
+++ models/invoice_item.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} InvoiceItemType
+ * @property {string} [id]
+ * @property {string} [end_date]
+ * @property {string} [task]
+ * @property {string} [description]
+ * @property {number} [unit_value]
+ * @property {number} [quantity]
+ * @property {number} [discount]
+ * */
