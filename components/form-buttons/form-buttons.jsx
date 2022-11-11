d2b5196debeb2f697786159a5daa638b29275019
Manuel Vega <manuelvega.dev@gmail.com>
2022-11-10 23:23:04 -0500

    feat(components): [FormButtons] Edited component trying to follow the Carbon Design System

--- components/form-buttons/form-buttons.jsx
+++ components/form-buttons/form-buttons.jsx
@@ -1,26 +1,33 @@
 import { Button, InlineLoading } from "@carbon/react";
 
-export function FormButtons({
+export const FormButtons = ({
   isSubmitting,
   success,
-  description,
-}) {
-  return (
-    <div style={{ display: 'flex' }}>
+  successMessage,
+  submitButtonText,
+  onSuccess,
+}) => (
+  <div style={{ display: "flex", gap: "1rem" }}>
+    {isSubmitting || success ? (
+      <InlineLoading
+        style={{ marginLeft: "1rem", height: "3rem" }}
+        description={
+          isSubmitting ? "Submitting..." : successMessage || "Success!"
+        }
+        status={success ? "finished" : "active"}
+        onSuccess={() => {
+          if (onSuccess) onSuccess();
+        }}
+      />
+    ) : (
+      <Button type="submit">{submitButtonText ?? "Submit"}</Button>
+    )}
+    {isSubmitting || success ? null : (
       <Button kind="secondary" disabled={isSubmitting || success}>
         Cancel
       </Button>
-      {isSubmitting || success ? (
-        <InlineLoading
-          style={{ marginLeft: '1rem' }}
-          description={description}
-          status={success ? 'finished' : 'active'}
-        />
-      ) : (
-        <Button type="submit">Submit</Button>
-      )}
-    </div>
-  );
-}
+    )}
+  </div>
+);
 
 export default FormButtons;
